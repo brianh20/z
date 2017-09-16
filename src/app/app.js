a39ab39a4b06bf2754d5bea00c2c673449b64698
unknown <brian.hume@live.com>
2017-09-16 15:43:52 -0300

    Transformation in process

--- src/app/app.js
+++ src/app/app.js
@@ -9,45 +9,21 @@ app.config(function($stateProvider){
 		templateUrl : '/app/pages/home.html'
 	}
 
-	var newSensor = {
-		name: 'newSensor',
-		url: '/newSensor',
-		templateUrl : '/app/pages/newSensor.html'
+	var cardiologo = {
+		name: 'cardiologo',
+		url: '/cardiologo',
+		templateUrl : '/app/pages/cardiologo.html'
 	}
 
-	var newElement = {
-		name: 'newElement',
-		url: '/newElement',
-		templateUrl : '/app/pages/newElement.html'
+	var laboratorio = {
+		name: 'laboratorio',
+		url: '/laboratorio',
+		templateUrl : '/app/pages/laboratorio.html'
 	}
-
-	var viewSensors = {
-		name: 'viewSensors',
-		url: '/viewSensors',
-		templateUrl : '/app/pages/viewSensors.html'
-	}
-
-	var viewElements = {
-		name: 'viewElements',
-		url: '/viewElements',
-		templateUrl : '/app/pages/viewElements.html'
-	}
-
-	var editElement = {
-		name: 'editElement',
-		url: '/editElement',
-		templateUrl : '/app/pages/editElement.html'
-	}
-
-
 
 	$stateProvider.state(home);
-	$stateProvider.state(newSensor);
-	$stateProvider.state(newElement);
-	$stateProvider.state(viewSensors);
-	$stateProvider.state(viewElements);
-	$stateProvider.state(editElement);
-	
+	$stateProvider.state(cardiologo);
+	$stateProvider.state(laboratorio);
 });
 
 app.directive('navbar', function(){
@@ -56,31 +32,31 @@ app.directive('navbar', function(){
   };
 });
 
-app.controller('editElement', function($scope) {
+// app.controller('editElement', function($scope) {
 
-	angular.element(document).ready(function () {
-		var expected = {lat: -34.6160073, lng: -58.3890407};
-		var truth = {lat: -34.617, lng: -58.3990407};
+// 	angular.element(document).ready(function () {
+// 		var expected = {lat: -34.6160073, lng: -58.3890407};
+// 		var truth = {lat: -34.617, lng: -58.3990407};
 
-		var map = new google.maps.Map(document.getElementById('elementMap'), {
-	  		zoom: 14,
-	  		center: expected
-		});
+// 		var map = new google.maps.Map(document.getElementById('elementMap'), {
+// 	  		zoom: 14,
+// 	  		center: expected
+// 		});
 		
-		var markerExpected = new google.maps.Marker({
-	  		position: expected,
-	  		map: map,
-	  		icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
-		});
+// 		var markerExpected = new google.maps.Marker({
+// 	  		position: expected,
+// 	  		map: map,
+// 	  		icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png'
+// 		});
 		
-		var markerTruth = new google.maps.Marker({
-	  		position: truth,
-	  		map: map,
-	  		icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
-		});
-    });
+// 		var markerTruth = new google.maps.Marker({
+// 	  		position: truth,
+// 	  		map: map,
+// 	  		icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png'
+// 		});
+//     });
 
-});
+// });
 
 
 
